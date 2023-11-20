6edad29c2d3f94daa8214a05cce9904e788b388a
Mohammad Ahsan <124549647+mahsan2606@users.noreply.github.com>
2023-11-20 13:21:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ecommerce-backend
+ Backend with Folder Structure
---
+++ routes/tourRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
+ 
+ 
+ 
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
+
+module.exports = router;
